172697ba8ed253bf79c2a30f124807dd226d2996
utsubo256 <greeeentea256@gmail.com>
2025-02-10 21:56:55 +0900

    メモ詳細ページを作成

--- app.rb
+++ app.rb
@@ -7,3 +7,9 @@ get '/memos' do
   @memos = json[:memos]
   erb :'books/index'
 end
+
+get '/memos/:memo_id' do
+  json = JSON.parse(File.read('./data/memos.json'), symbolize_names: true)
+  @memo = json[:memos].find { |memo| memo[:id] == params[:memo_id].to_i }
+  erb :'books/show'
+end
