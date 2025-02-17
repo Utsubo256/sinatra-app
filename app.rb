7a8a45381113e07561802bdb6eff04495036b332
utsubo256 <greeeentea256@gmail.com>
2025-02-17 16:46:03 +0900

    jsonからのデータ読み込み、対象のメモを見つけるための処理をメソッドに切り出し

--- app.rb
+++ app.rb
@@ -4,8 +4,16 @@ require 'sinatra'
 
 MEMO_PATH = './data/memos.json'
 
+def load_memo_data
+  JSON.parse(File.read(MEMO_PATH), symbolize_names: true)
+end
+
 def sanitize(text)
   Rack::Utils.escape_html(text)
+end
+
+def find_memo(memos, target_id)
+  memos.find { |memo| memo[:id] == target_id }
 end
 
 get '/' do
@@ -13,7 +21,7 @@ get '/' do
 end
 
 get '/memos' do
-  json = JSON.parse(File.read(MEMO_PATH), symbolize_names: true)
+  json = load_memo_data
   @memos = json[:memos]
   erb :'books/index'
 end
@@ -23,13 +31,13 @@ get '/memos/new' do
 end
 
 get '/memos/:memo_id' do
-  json = JSON.parse(File.read(MEMO_PATH), symbolize_names: true)
-  @memo = json[:memos].find { |memo| memo[:id] == params[:memo_id].to_i }
+  json = load_memo_data
+  @memo = find_memo(json[:memos], params[:memo_id].to_i)
   erb :'books/show'
 end
 
 post '/memos' do
-  json = JSON.parse(File.read(MEMO_PATH), symbolize_names: true)
+  json = load_memo_data
   adding_memo = { id: json[:id_counter] + 1, title: sanitize(params[:title]), description: sanitize(params[:description]) }
   result_json = { id_counter: json[:id_counter] + 1, memos: [*json[:memos], adding_memo] }
   File.write(MEMO_PATH, JSON.dump(result_json))
@@ -37,13 +45,13 @@ post '/memos' do
 end
 
 get '/memos/:memo_id/edit' do
-  json = JSON.parse(File.read(MEMO_PATH), symbolize_names: true)
-  @memo = json[:memos].find { |memo| memo[:id] == params[:memo_id].to_i }
+  json = load_memo_data
+  @memo = find_memo(json[:memos], params[:memo_id].to_i)
   erb :'books/edit'
 end
 
 patch '/memos/:memo_id' do
-  json = JSON.parse(File.read(MEMO_PATH), symbolize_names: true)
+  json = load_memo_data
   result_json = json
   target_index = result_json[:memos].find_index { |memo| memo[:id] == params[:memo_id].to_i }
   updating_memo = { id: result_json[:memos][target_index][:id], title: sanitize(params[:title]), description: sanitize(params[:description]) }
@@ -53,7 +61,7 @@ patch '/memos/:memo_id' do
 end
 
 delete '/memos/:memo_id' do
-  json = JSON.parse(File.read(MEMO_PATH), symbolize_names: true)
+  json = load_memo_data
   remaining_memos = json[:memos].reject { |memo| memo[:id] == params[:memo_id].to_i }
   result_json = { id_counter: json[:id_counter] + 1, memos: remaining_memos }
   File.write(MEMO_PATH, JSON.dump(result_json))
